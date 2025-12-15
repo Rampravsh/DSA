158e8cc16a10925b5cd073405ec4dd13915d6b51
Rampravesh <rampraveshkr4545@gmail.com>
2025-12-15 15:52:29 +0530

    Add initial implementations for array manipulation and basic arithmetic operations

---
+++ Array/app.js
@@ -0,0 +1,8 @@
+let arr = [];
+arr.push(100);
+arr.push(false);
+arr.push("rampravesh");
+arr.push(100);
+
+console.log(arr);
+console.log(arr[1]);
---
+++ Master the Basic/index.js
@@ -0,0 +1,3 @@
+let a = 10;
+let b = 39;
+console.log("sum of a and b is", a + b);

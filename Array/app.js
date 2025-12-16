e212bed2aa0bd94e6dc00eaead5f54ade32e1dc3
Rampravesh <rampraveshkr4545@gmail.com>
2025-12-16 20:58:42 +0530

    Refactor app.js to comment out existing code and add index.html for structure

--- Array/app.js
+++ Array/app.js
@@ -1,8 +1,85 @@
-let arr = [];
-arr.push(100);
-arr.push(false);
-arr.push("rampravesh");
-arr.push(100);
+// let arr = [];
+// arr.push(100);
+// arr.push(false);
+// arr.push("rampravesh");
+// arr.push(100);
 
-console.log(arr);
-console.log(arr[1]);
+// arr[8] = 34;
+// console.log(arr);
+// console.log(arr[4]);
+
+// let newArr = new Array(4);
+// console.log(newArr);
+
+// let arr = [];
+// arr[0] = Number(prompt("enter a number"));
+// arr[1] = Number(prompt("enter a number"));
+// arr[2] = Number(prompt("enter a number"));
+// arr[3] = Number(prompt("enter a number"));
+// arr[4] = Number(prompt("enter a number"));
+// console.log(arr);
+
+// let arr = new Array(5);
+// for (let i = 0; i < arr.length; i++) {
+//   arr[i] = Number(prompt("enter a number"));
+// }
+// console.log(arr);
+
+// let arr = [10, 20, 30, 40, 50];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   sum = sum + arr[i];
+// }
+// console.log(sum);
+
+// let arr = [24, 5, 3, 236, 6];
+// let max = arr[0];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] > max) {
+//     max = arr[i];
+//   }
+// }
+// console.log(max);
+
+// let min = arr[0];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] < min) {
+//     min = arr[i];
+//   }
+// }
+// console.log(min);
+
+// let arr = [5, 6];
+// let max = Number.MIN_SAFE_INTEGER;
+// let secMax = Number.MIN_SAFE_INTEGER;
+// for (let i = 0; i < arr.length; i++) {
+//   console.log("===============\n");
+//   if (arr[i] > max) {
+//     console.log(i);
+//     secMax = max;
+//     max = arr[i];
+//     console.log("max:", max);
+//     console.log("secmax:", secMax);
+//   } else if (arr[i] > secMax) {
+//     console.log(i);
+//     secMax = arr[i];
+//     console.log("elseif bala secmax:", secMax);
+//   }
+// }
+// console.log(max);
+// console.log(secMax);
+
+// let arr = [53, 2, 5, 242, 54, 64];
+
+// let min = Number.MAX_SAFE_INTEGER;
+// let secMin = Number.MAX_SAFE_INTEGER;
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] < min) {
+//     secMin = min;
+//     min = arr[i];
+//   } else if (arr[i] < secMin) {
+//     secMin = arr[i];
+//   }
+// }
+// console.log(secMin);

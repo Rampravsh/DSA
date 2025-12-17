41ef4d2d210f931109688d9e889b13a4470eaa51
Rampravesh <rampraveshkr4545@gmail.com>
2025-12-17 21:59:31 +0530

    Update array examples and implement array reversal algorithms in app.js; add header in index.html

--- Array/app.js
+++ Array/app.js
@@ -49,7 +49,7 @@
 // }
 // console.log(min);
 
-// let arr = [5, 6];
+// let arr = [53, 2, 5, 242, 54, 64, 64, 242, 2];
 // let max = Number.MIN_SAFE_INTEGER;
 // let secMax = Number.MIN_SAFE_INTEGER;
 // for (let i = 0; i < arr.length; i++) {
@@ -60,7 +60,7 @@
 //     max = arr[i];
 //     console.log("max:", max);
 //     console.log("secmax:", secMax);
-//   } else if (arr[i] > secMax) {
+//   } else if (arr[i] > secMax && arr[i] < max) {
 //     console.log(i);
 //     secMax = arr[i];
 //     console.log("elseif bala secmax:", secMax);
@@ -69,7 +69,7 @@
 // console.log(max);
 // console.log(secMax);
 
-// let arr = [53, 2, 5, 242, 54, 64];
+// let arr = [53, 2, 5, 242, 54, 64, 64, 242, 2];
 
 // let min = Number.MAX_SAFE_INTEGER;
 // let secMin = Number.MAX_SAFE_INTEGER;
@@ -82,4 +82,64 @@
 //     secMin = arr[i];
 //   }
 // }
+// console.log(min);
 // console.log(secMin);
+
+//✅✅✅ reverse the array with extra space
+
+// let arr = [21, 43, 65, 68, 42, 86, 44, 54];
+
+// let temp = new Array(arr.length);
+// let j = 0;
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   //   temp.push(arr[i]);  // not push new elemene because after push new element in temp size increase
+//   temp[j] = arr[i];
+//   j++;
+// }
+
+// console.log(temp);
+
+//✅✅✅ reverse the array without extra spaces
+
+// let arr = [21, 43, 65, 42, 86, 44, 54];
+// // console.log(arr);
+// let j = 0;
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   if (i >= j) {
+//     let temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+//     //   [arr[i], arr[j]] = [arr[j], arr[i]];
+//   }
+//   j++;
+// }
+
+// console.log("last ans", arr);
+
+//✅✅✅ TWO POINTER
+
+// let arr = [1, 1, 0, 1, 0, 1, 1, 0, 0];
+// let j = 0;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] == 0) {
+//     let temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+//     // console.log(arr)
+//     j++;
+//   }
+// }
+
+// console.log(arr);
+
+let arr = [-2, 32, 34, -46, 3, 5, -342, -3, 64];
+
+let j = 0;
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] < 0) {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+    j++;
+  }
+}
+
+console.log(arr);

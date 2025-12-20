6be4100fed9ccf08cb47081be090dea472c9361e
Rampravesh <rampraveshkr4545@gmail.com>
2025-12-20 16:29:31 +0530

    comment out prefix sum function and example usage for clarity

--- Prefix Sum/index.js
+++ Prefix Sum/index.js
@@ -3,13 +3,17 @@
 //❓❓❓Given an integer array 'a' , return the prefix sum/running sum in
 // the same array without creating a new array
 
-let arr = [2, 3, 4, 2, 5, 1, 19];
-function prefixSum() {
-  let n = arr.length;
-  for (let i = 1; i < n; i++) {
-    arr[i] = arr[i] + arr[i - 1];
-  }
-  return arr;
-}
+// let arr = [2, 3, 4, 2, 5, 1, 19];
+// function prefixSum() {
+//   let n = arr.length;
+//   for (let i = 1; i < n; i++) {
+//     arr[i] = arr[i] + arr[i - 1];
+//   }
+//   return arr;
+// }
 
-console.log(prefixSum(arr));
+// console.log(prefixSum(arr));
+
+//❓❓❓ Given an array of integers of size n. Answer q queries where you need to print the sum
+//  of values in a given range of indices from 1 to r (both included).
+// 📝📝📝 The values of 1 and r in queries follow 1-based indexing.

b609daf97f97ad007783edc8ba60bcaa4af3b80c
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-16 10:30:19 +0530

    decimal to binary

--- DSA with PW/Bit manipulation/decimal_to_binary.cpp
+++ DSA with PW/Bit manipulation/decimal_to_binary.cpp
@@ -14,30 +14,15 @@ string decimalToBinary(int n)
     return binary;
 }
 
-int binaryToDecimal(string binary){
-    int decimal=0;
-    int p=1;
-    int n=binary.size();
-    for(int i =n-1;i>=0;i--){
-        if(binary[i]=='1'){
-            decimal+=p;
-        }
-        p*=2;
-    }
-    return decimal;
 
-}
 
 int main()
 {
-    // int n;
-    // cout<<"Enter number: ";
-    // cin >> n;
-    // cout<<decimalToBinary(n)<<endl;
+    int n;
+    cout<<"Enter number: ";
+    cin >> n;
+    cout<<decimalToBinary(n)<<endl;
 
-    string binary;
-    cout << "enter your binary: ";
-    cin>>binary;
-    cout<<binaryToDecimal(binary)<<endl;
+
     return 0;
 }

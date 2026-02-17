6719952645e969f76271c69dbf9af1fcecb7ab20
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-17 12:35:50 +0530

    sum of array using recursion

--- Hitesh sir/Recursion/indexl.js
+++ Hitesh sir/Recursion/indexl.js
@@ -1,38 +1,41 @@
 // Q Print numbers from Lrange to Urange using recursion
 
-function printNumber(lrange, urange) {
-  if (lrange >= urange) {
-    return;
-  }
-  console.log(lrange);
-  printNumber(lrange + 1, urange);
-}
+// function printNumber(lrange, urange) {
+//   if (lrange >= urange) {
+//     return;
+//   }
+//   console.log(lrange);
+//   printNumber(lrange + 1, urange);
+// }
 
-console.log(printNumber(2, 8));
+// console.log(printNumber(2, 8));
 
 // Q Print numbers from Urange to lrange using recursion
 
-function printNumber2(lrange, urange) {
-  if (lrange >= urange) {
-    return;
-  }
-  printNumber2(lrange + 1, urange);
-  console.log(lrange);
-}
+// function printNumber2(lrange, urange) {
+//   if (lrange >= urange) {
+//     return;
+//   }
+//   printNumber2(lrange + 1, urange);
+//   console.log(lrange);
+// }
 
 // console.log(printNumber2(2, 8));
 
-// Q. sum array using recursion 
+// Q. sum array using recursion
 
-let sum =0
-function sumRecursive(arr){
-    
-    if(arr.length===0){
-        return
-    }
-    
+// let sum = 0;
+// function sumRecursive(arr, i) {
+//   if (i === arr.length) {
+//     return;
+//   }
+//   sum += arr[i];
+//   sumRecursive(arr,i+1)
+// }
 
-}
+// arr=[1,2,3,4,5,5,6]
 
+// sumRecursive(arr,0)
 
+// console.log(sum)
 

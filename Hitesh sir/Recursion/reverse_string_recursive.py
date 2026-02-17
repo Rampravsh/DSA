78e361be7a43cc6bf7608fa6503565cd5c5fafcf
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-17 13:06:28 +0530

    just exeperment

--- Hitesh sir/Recursion/reverse_string_recursive.py
+++ Hitesh sir/Recursion/reverse_string_recursive.py
@@ -1,7 +1,26 @@
-def recursive_reverse_string(s):
-    if len(s)<=1:
-        return s
-    return recursive_reverse_string(s[1:])+s[0]
+# def recursive_reverse_string(s):
+#     if len(s)<=1:
+#         return s
+#     return recursive_reverse_string(s[1:])+s[0]
     
 
-print(recursive_reverse_string("hello"))
+# print(recursive_reverse_string("hello"))
+
+# secong method 
+
+new_string = ''
+
+def recursive_reverse_string(s, i):
+    global new_string
+    # बेस केस: जब स्ट्रिंग खत्म हो जाए
+    if i == len(s):
+        return
+
+    # पहले आगे की रिकर्सिव कॉल करें
+    recursive_reverse_string(s, i + 1)
+    
+    # रिकर्शन से लौटते समय कैरेक्टर जोड़ें
+    new_string += s[i]
+
+recursive_reverse_string("hello", 0)
+print(new_string) # अब आउटपुट आएगा: olleh

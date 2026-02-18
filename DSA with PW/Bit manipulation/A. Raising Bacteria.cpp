1b1590259f10d7cb829412accc2e1c1732137037
Rampravesh <rampraveshkr4545@gmail.com>
2026-02-18 12:55:04 +0530

    raising bacteria

---
+++ DSA with PW/Bit manipulation/A. Raising Bacteria.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+long long countSetBits(long long n){
+    long long count =0;
+    while(n){
+        n=n&(n-1);
+        count++;
+    }
+    return count;
+}
+
+int main() {
+    long long x;
+    cin>>x;
+    cout<<countSetBits(x);
+    return 0;
+}
